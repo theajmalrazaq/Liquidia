2720e148dd43f7a38e7516205c383679a60ce707
Ajmal Razaq Bhatti <127989125+theajmalrazaq@users.noreply.github.com>
2024-10-20 10:50:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 console.log("first");
 //global variables
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
+
 let selectedCurrency = "USD"; //default value for currency
 
 // Theme toggle functionality
